50c63b04063c4bccb000616667897faf609b98c9
Manu <mcufre84@gmail.com>
2021-05-29 17:53:42 +0200

    fixing refresh issue

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import React from "react";
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
 import Join from "./components/join/Join";
 import Chat from "./components/chat/Chat";
 
 const App = () => (
   <Router>
-    <Route path="/" exact component={Join} />
-    <Route path="/chat" component={Chat} />
+    <Switch>
+      <Route path="/" exact component={Join} />
+      <Route path="/chat" component={Chat} />
+    </Switch>
   </Router>
 );
 
